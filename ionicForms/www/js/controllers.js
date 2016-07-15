2cf1a921a5aeadb65b1c459ad147baff5d079345
Gonzalo Di Giovanni <gonza.digiovanni@gmail.com>
2016-07-15 17:58:24 -0300

    Se comienza con la implementacion del form

---
+++ ionicForms/www/js/controllers.js
@@ -0,0 +1,5 @@
+angular.module('controllers', [])
+
+.controller('FormCtrl', function($scope, $state) {
+  
+})
---
+++ ionicForms/www/js/services.js
@@ -0,0 +1,5 @@
+angular.module('services', [])
+
+.service('FormService', function() {
+
+})
