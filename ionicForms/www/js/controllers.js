b7e53eec789f03d86d6f1eaeb6c942514735fc96
Gonzalo Di Giovanni <gonza.digiovanni@gmail.com>
2016-07-19 17:47:07 -0300

    Se agrega username con todas sus validaciones

--- ionicForms/www/js/controllers.js
+++ ionicForms/www/js/controllers.js
@@ -1,5 +1,27 @@
 angular.module('controllers', [])
 
 .controller('FormCtrl', function($scope, $state) {
-  
+  $scope.user ={ sex: 'Male'};
+
+  $scope.areEquals = function(pass, confirm_pass){
+    return pass!=confirm_pass;
+  }
+})
+
+.directive('validUsername',function(){
+  return{
+    require: 'ngModel',
+    link: function(scope, ele, attrs, c){
+      scope.$watch(attrs.ngModel, function(username_value){
+        if(username_value=="abc123" || username_value=="123abc")
+        {
+         c.$setValidity('validusername', false);
+        }
+        else
+        {
+         c.$setValidity('validusername', true);
+        }
+      });
+    }
+  };
 })
