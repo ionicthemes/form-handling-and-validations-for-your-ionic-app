d9ca8ff910de397bca0ee45236e299feb18a980f
Gonzalo Di Giovanni <gonza.digiovanni@gmail.com>
2016-07-21 14:50:01 -0300

    Se arregla error en el ng-disabled del boton submit

--- ionicForms/www/js/controllers.js
+++ ionicForms/www/js/controllers.js
@@ -1,10 +1,29 @@
 angular.module('controllers', [])
 
 .controller('FormCtrl', function($scope, $state) {
-  $scope.user ={ sex: 'Male'};
+  $scope.user ={
+    sex: 'Male',
+    username: '',
+    name: '',
+    surname: '',
+    email: '',
+    password: '',
+    confirm_password: ''};
+  $scope.match_password = "false";
 
   $scope.areEquals = function(pass, confirm_pass){
     return pass!=confirm_pass;
+  }
+
+  $scope.submit = function(user){
+    var pass = user.password,
+        confirm_pass = user.confirm_password;
+    if(pass != confirm_pass){
+      $scope.match_password = "true";
+    }
+    else{
+      $scope.match_password = "false";
+    }
   }
 })
 
