b8a4a0f7e656ba33d468c4dfcd5551c8baf9e947
Gonzalo Di Giovanni <gonza.digiovanni@gmail.com>
2016-08-02 11:46:37 -0300

    Se arregla un bug en mensaje de error de match password y se agrega mensaje de error en accept terms and conditions

--- ionicForms/www/js/controllers.js
+++ ionicForms/www/js/controllers.js
@@ -15,10 +15,16 @@ angular.module('controllers', [])
     var pass = user.password,
         confirm_pass = user.confirm_password;
     if(pass != confirm_pass){
-      $scope.match_password = "true";
+      $scope.not_match_password = "true";
     }
     else{
-      $scope.match_password = "false";
+      $scope.not_match_password = "";
+    }
+    if(user.accept){
+      $scope.not_accept_terms = "";
+    }
+    else{
+      $scope.not_accept_terms = "true";
     }
   }
 })
